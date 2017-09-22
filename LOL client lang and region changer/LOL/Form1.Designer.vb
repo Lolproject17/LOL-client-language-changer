66405ba35cccdd8693ae7b890a89a370f15a2087
Lolproject17 <32173070+Lolproject17@users.noreply.github.com>
2017-09-22 20:14:25 +0300

    Add files via upload

---
+++ LOL client lang and region changer/LOL/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(43, 73)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(70, 24)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(43, 178)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(43, 122)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(70, 24)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(395, 23)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(28, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(43, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(346, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(440, 236)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LOL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ LOL client lang and region changer/LOL/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim region As String = ""
+    Dim language As String = ""
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("BR")
+        ComboBox1.Items.Add("EUNE")
+        ComboBox1.Items.Add("EUW")
+        ComboBox1.Items.Add("JP")
+        ComboBox1.Items.Add("LA1")
+        ComboBox1.Items.Add("LA2")
+        ComboBox1.Items.Add("NA")
+        ComboBox1.Items.Add("OC1")
+        ComboBox1.Items.Add("RU")
+        ComboBox1.Items.Add("TR")
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+
+        ComboBox2.Items.Add("en_US")
+        ComboBox2.Items.Add("pt_BR")
+        ComboBox2.Items.Add("tr_TR")
+        ComboBox2.Items.Add("en_GB")
+        ComboBox2.Items.Add("es_ES")
+        ComboBox2.Items.Add("fr_FR")
+        ComboBox2.Items.Add("it_IT")
+        ComboBox2.Items.Add("cs_CZ")
+        ComboBox2.Items.Add("el_GR")
+        ComboBox2.Items.Add("hu_HU")
+        ComboBox2.Items.Add("pl_PL")
+        ComboBox2.Items.Add("ro_RO")
+        ComboBox2.Items.Add("ru_RU")
+        ComboBox2.Items.Add("es_MX")
+        ComboBox2.Items.Add("en_AU")
+        ComboBox2.Items.Add("ja_JP")
+        ComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+
+        My.Settings.txtpath = ""
+        TextBox1.Text = My.Settings.filepath
+        st()
+        check() ' if correct path 
+
+
+    End Sub
+
+    Sub check()
+        Try
+
+            For Each line As String In IO.File.ReadAllLines(My.Settings.txtpath)
+                If line.Contains("region: ") Then
+                    region = line.Split("""")(1)
+                    ComboBox1.SelectedItem = region
+                End If
+                If line.Contains("locale: ") Then
+                    language = line.Split("""")(1)
+                    ComboBox2.SelectedItem = language
+                End If
+            Next
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Private Sub ComboBox1_TextChanged(sender As Object, e As EventArgs) Handles ComboBox1.TextChanged
+        Try
+            My.Computer.FileSystem.WriteAllText(My.Settings.txtpath, My.Computer.FileSystem.ReadAllText(My.Settings.txtpath).Replace(region, ComboBox1.SelectedItem), False)
+            region = ComboBox1.SelectedItem
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox2_TextChanged(sender As Object, e As EventArgs) Handles ComboBox2.TextChanged
+        Try
+            My.Computer.FileSystem.WriteAllText(My.Settings.txtpath, My.Computer.FileSystem.ReadAllText(My.Settings.txtpath).Replace(language, ComboBox2.SelectedItem), False)
+            language = ComboBox2.SelectedItem
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Process.Start(TextBox1.Text)
+            Me.Close()
+        Catch ex As Exception
+            MsgBox("Probably the file path is wrong!", MsgBoxStyle.Critical)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        If openFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK AndAlso openFileDialog1.FileName <> "" Then
+            My.Settings.filepath = System.IO.Path.GetFullPath(openFileDialog1.FileName)
+            TextBox1.Text = My.Settings.filepath
+        End If
+
+        st()
+
+        check()
+
+    End Sub
+
+    Sub st()
+        Dim A As String() = Split(TextBox1.Text, "\")
+        Dim pa As String = ""
+        For Each k As String In A
+            If k <> "LeagueClient.exe" Then
+                pa = pa + k + "\"
+            End If
+
+        Next
+        My.Settings.txtpath = pa + "Config\LeagueClientSettings.yaml"
+
+    End Sub
+
+
+End Class
